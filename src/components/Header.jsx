e3e9f5193b98cece413f5fe7fb990f7a1441dc03
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-11-02 14:57:31 +0100

    git init v1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <div>
+                <h1>ToDolist</h1>
+                <div>
+                    <nav></nav>
+                </div>
+            </div>
+        </header>
+    );
+}
+export { Header };
